5bcba7e0c37958033a86fabe6f4ac7d5cecef829
deoracle <dev@deoracle.io>
2025-01-23 12:14:36 +0800

    fix solana address

--- deoracle_proof/proof.py
+++ deoracle_proof/proof.py
@@ -6,6 +6,7 @@ from typing import Dict, Any
 import requests
 import json
 import hashlib
+import base58
 from base64 import b64decode
 
 from deoracle_proof.models.proof_response import ProofResponse
@@ -54,11 +55,10 @@ class Proof:
         if ownership and data_contract != "":
             data_chain = data_chain.lower()
             if data_chain == "solana":
-                if len(data_contract) == 44:
+                if is_valid_solana_address(data_contract):
                     total_supply = get_total_supply_solana(networks[data_chain], data_contract)
-                    logging.info("sol supply {}".format(total_supply))
             elif data_chain == "eth" or data_chain == "base" or data_chain == "vana" :
-                if len(data_contract) == 42:
+                if len(data_contract) == 42 and data_contract.lower().startswith("0x"):
                     total_supply = get_total_supply_evm(networks[data_chain], data_contract)
                     
         logging.info( "{} on {} supply {}".format(data_contract, data_chain, total_supply))
@@ -119,6 +119,8 @@ def get_total_supply_evm(rpc_url, token_address):
         result = response.json()
 
         total_supply_hex = result.get("result")
+        if total_supply_hex == "0x":
+            total_supply_hex = "0x0"
         if total_supply_hex:
             total_supply = int(total_supply_hex, 16)
             return total_supply
@@ -145,32 +147,47 @@ def get_total_supply_solana(rpc_url, token_mint_address):
 
         account_info = result.get("result", {}).get("value")
         if account_info is None:
-            logging.error( f"Error: Mint account {token_mint_address} not found.")
+            logging.info( f"Error: Mint account {token_mint_address} not found.")
             return 0
 
         account_data = account_info["data"][0]
         decoded_data = b64decode(account_data)
 
         if len(decoded_data) < 82:
-            logging.error("account not mint, invalid length")
+            logging.info("account not mint, invalid length")
             return 0
         
         
         decimals = decoded_data[44]
         is_initialized = decoded_data[45] == 1
         if not is_initialized or decimals <= 0 or decimals > 18:
-            logging.error(f"account not mint, init {is_initialized} decimals {decimals}")
+            logging.info(f"account not mint, init {is_initialized} decimals {decimals}")
             return 0
         
         # Mint Account 
         mint_supply = int.from_bytes(decoded_data[36:44], "little") 
-        # decimals = decoded_data[44] 
-        # total_supply = mint_supply / (10 ** decimals)
-        # return total_supply
+        # logging.info(f"sol mint, init {is_initialized} decimals {decimals}, supply {mint_supply}")
         return mint_supply
     except Exception as e:
         logging.error("request error: %s", str(e), exc_info=True)
         return 0
+    
+def is_valid_solana_address(address):
+    if len(address) < 32 or len(address) > 44:
+        return False
+    try:
+        
+        # 尝试解码地址
+        decoded = base58.b58decode(address)
+        
+        # 检查解码后的字节长度是否为 32 字节
+        if len(decoded) == 32:
+            return True
+        else:
+            return False
+    except Exception as e:
+        # 如果解码失败，说明不是有效的 Base58 地址
+        return False
   
 def check_hash_repeat(rpc_url, hash):
     payload = {
